88ad062f48ee5ac028f2645f8e47f61ce2c69954
Daniel <avelar42@hotmail.com>
2022-08-02 22:43:56 -0300

    Adjust in categories

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen(this.category);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Receitas"), actions: []),
+      appBar: AppBar(title: Text(category.title), actions: []),
       body: Center(
-        child: Text("Receitas por categoria"),
+        child: Text("Receitas por categoria ${category.id}"),
       ),
     );
   }
